e3aeca1c376b052f3bb19075a991479d9aac8216
root <root@Leonardo-PC.localdomain>
2021-07-28 14:58:31 -0300

    primeiro commit

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-03d5c68bab01f3496"
   instance_type = "t2.micro"
-
+  key_name = "iac-alura"
   tags = {
     Name = "Primeira instancia"
   }
